78679e4e0ce9bbd743ef8f977022408ab348d5aa
Kashyap 2210 <kash.cdac@gmail.com>
2024-12-07 17:43:24 +0530

    user repo delete

--- src/users/user.repository.ts
+++
@@ -1,6 +0,0 @@
-import { Repository } from 'typeorm';
-import { UserEntity } from './user.entity';
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class UserEntityRepository extends Repository<UserEntity> {}

09d653b3d3d991500bcb11224119152d835f0e26
Kashyap 2210 <kash.cdac@gmail.com>
2024-12-29 14:11:44 +0530

    changes in the validatePresence & getByFilter method

--- src/helpers/entity.repository.ts
+++ src/helpers/entity.repository.ts
@@ -20,55 +20,95 @@ export abstract class EntityManagerBaseService<T> {
     return repository.createQueryBuilder(repository.metadata.tableName);
   }
 
+  // async validatePresence<P>(
+  //   propertyName: string,
+  //   propertyValue: P[],
+  //   key?: string,
+  //   entityManager?: EntityManager,
+  // ): Promise<T[]> {
+  //   const repository = this.getRepository(entityManager);
+  //   const tableName = repository.metadata.tableName;
+  //   let query = repository.createQueryBuilder(tableName);
+
+  //   query = query.andWhere(
+  //     `${tableName}.${propertyName} IN (:...${propertyName}){
+  //         ${propertyName}:${propertyValue}
+  //         }`,
+  //   );
+  //   // console.log(query, 'this is the query');
+  //   const entities = await query.getMany();
+  //   // Find missing values
+  //   const existingValues = entities.map((entity) => entity[propertyName]);
+  //   const missingValues = propertyValue.filter(
+  //     (value) => !existingValues.includes(value),
+  //   );
+
+  //   // If there are missing values, throw an error
+  //   if (missingValues.length > 0) {
+  //     throw new BadRequestException({
+  //       key: key || propertyName,
+  //       message: `${tableName.charAt(0).toUpperCase() + tableName.slice(1)} with ${propertyName} ${missingValues.join(
+  //         ', ',
+  //       )} not found.`,
+  //     });
+  //   }
+  //   return entities;
+  // }
+
+  // async getById<P>(id: number, entityManager?: EntityManager): Promise<T[]> {
+  //   const repository = this.getRepository(entityManager);
+  //   const tableName = repository.metadata.tableName;
+  //   let query = repository.createQueryBuilder(tableName);
+
+  //   query = query.andWhere(`${tableName}.${id} IN (:...${id})`, {
+  //     [id]: id,
+  //   });
+  //   const entity = await query.getMany();
+  //   if (entity.length === 0) {
+  //     throw new BadRequestException({
+  //       key: id,
+  //       message: `${tableName.charAt(0).toUpperCase()} entity with ${id} not found`,
+  //     });
+  //   }
+  //   return entity;
+  // }
+
   async validatePresence<P>(
     propertyName: string,
-    propertyValue: P[],
+    propertyValues: P[], // Array of values to check
     key?: string,
     entityManager?: EntityManager,
   ): Promise<T[]> {
     const repository = this.getRepository(entityManager);
     const tableName = repository.metadata.tableName;
-    let query = repository.createQueryBuilder(tableName);
 
-    query = query.andWhere(
-      `${tableName}.${propertyName} IN (:...${propertyName}){
-          [propertyName]:propertyValue
-          }`,
-    );
+    // Correctly parameterize the query using the IN clause
+    const query = repository
+      .createQueryBuilder(tableName)
+      .andWhere(`${tableName}.${propertyName} IN (:...propertyValues)`, {
+        propertyValues, // Array of property values to check
+      });
+
+    // Execute the query
     const entities = await query.getMany();
-    const propertyValuesFromEntities = entities.map(
-      (entity) => entity[propertyName],
-    );
-    const includes = propertyValuesFromEntities.includes(propertyValue);
-    const missingValues = entities.filter(
-      (entity) => !propertyValue.includes(entity[propertyName as keyof T] as P),
+
+    // Find missing values that don't exist in the entities
+    const existingValues = entities.map((entity) => entity[propertyName]);
+    const missingValues = propertyValues.filter(
+      (value) => !existingValues.includes(value),
     );
 
-    if (!includes) {
+    // If there are missing values, throw an error
+    if (missingValues.length > 0) {
       throw new BadRequestException({
-        key: key || 'id',
-        message: `${tableName.charAt(0).toUpperCase() + tableName.slice(1)} with ${propertyName} ${missingValues.join(',')} not found`,
+        key: key || propertyName,
+        message: `${tableName.charAt(0).toUpperCase() + tableName.slice(1)} with ${propertyName} ${missingValues.join(
+          ', ',
+        )} not found.`,
       });
     }
+
     return entities;
-  }
-
-  async getById<P>(id: number, entityManager?: EntityManager): Promise<T[]> {
-    const repository = this.getRepository(entityManager);
-    const tableName = repository.metadata.tableName;
-    let query = repository.createQueryBuilder(tableName);
-
-    query = query.andWhere(`${tableName}.${id} IN (:...${id})`, {
-      [id]: id,
-    });
-    const entity = await query.getMany();
-    if (entity.length === 0) {
-      throw new BadRequestException({
-        key: id,
-        message: `${tableName.charAt(0).toUpperCase()} entity with ${id} not found`,
-      });
-    }
-    return entity;
   }
 
   async deleteById<P>(
@@ -104,25 +144,14 @@ export abstract class EntityManagerBaseService<T> {
 
     // Loop through the filter object to dynamically build the query
     for (const [property, value] of Object.entries(filter)) {
-      // Use the `where` condition for each property dynamically
+      const normalizedValue = Array.isArray(value) ? value : [value];
       query = query.andWhere(`${tableName}.${property} IN (:...${property})`, {
-        [property]: value,
+        [property]: normalizedValue,
       });
     }
 
     // Execute the query and fetch the results
     const entities = await query.getMany();
-
-    // If no entities are found, throw a BadRequestException
-    if (entities.length === 0) {
-      throw new BadRequestException({
-        key: Object.keys(filter).join(', '),
-        message: `${tableName.charAt(0).toUpperCase() + tableName.slice(1)} with ${JSON.stringify(
-          filter,
-        )} not found`,
-      });
-    }
-
     return entities;
   }
 }

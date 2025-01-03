f55554965098ea06a8a89669f6b63f5923d3d130
Kashyap 2210 <kash.cdac@gmail.com>
2025-01-03 21:03:57 +0530

    cors policy add

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // Enable CORS globally
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(

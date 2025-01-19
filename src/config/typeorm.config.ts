9c57c64d9c028d9458da4218a68051579bc124ed
Kashyap Solanki <kash.cdac@gmail.com>
2025-01-19 14:14:10 +0530

    remove commented code

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -23,16 +23,6 @@ export default class TypeOrmConfig {
 }
 
 export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
-  //   type: 'mysql',
-  //   host: 'localhost',
-  //   port: 3306,
-  //   username: 'root',
-  //   password: 'sweetlordV12!',
-  //   database: 'blog_developement',
-  //   entities: [UserEntity, BlogEntity],
-  //   synchronize: true,
-  //   logging: true,
-
   useFactory: async (
     configService: ConfigService,
   ): Promise<TypeOrmModuleOptions> => TypeOrmConfig.getOrmConfig(configService),

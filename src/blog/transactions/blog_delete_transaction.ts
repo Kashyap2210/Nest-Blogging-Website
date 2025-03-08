4515ef153844f0ac1c39426fd568897b6ce27af1
Kashyap Solanki <kash.cdac@gmail.com>
2025-03-08 19:20:34 +0530

    Delete transaction enhance

--- src/blog/transactions/blog_delete_transaction.ts
+++ src/blog/transactions/blog_delete_transaction.ts
@@ -1,19 +1,58 @@
-import { BadRequestException, Inject } from '@nestjs/common';
+import { forwardRef, Inject } from '@nestjs/common';
 import { CommentsService } from '@src/comments/service/comments.service';
+import { BaseTransaction } from '@src/helpers/base.transaction';
 import { LikesCounterBlogsService } from '@src/likes-counter-blogs/services/likes-counter-blogs.service';
 import { IBlogLikesCounterEntity, ICommentEntity } from 'blog-common-1.0';
-import { DataSource } from 'typeorm';
+import { DataSource, EntityManager } from 'typeorm';
 import { BlogRepository } from '../repository/blogs.repository';
 import { IBlogDeleteData } from './interfaces/blog_entity_delete_transaction.interface';
 
-export class BlogDeleteTransaction {
+export class DeleteBlogWithinTransaction extends BaseTransaction<
+  IBlogDeleteData,
+  boolean
+> {
+  constructor(
+    @Inject(forwardRef(() => CommentsService))
+    private commentsService: CommentsService,
+    private likesDislikesService: LikesCounterBlogsService,
+    private blogRepository: BlogRepository,
+    @Inject(DataSource) dataSource: DataSource,
+  ) {
+    super(dataSource);
+  }
+
+  protected async execute(
+    data: IBlogDeleteData,
+    manager: EntityManager,
+  ): Promise<boolean> {
+    const { blogId, commentIds, likeDislikeEntityIds } = data;
+
+    if (commentIds.length > 0) {
+      await this.commentsService.deleteMany<ICommentEntity>(
+        commentIds,
+        manager,
+      );
+    }
+
+    if (likeDislikeEntityIds.length > 0) {
+      await this.likesDislikesService.deleteMany<IBlogLikesCounterEntity>(
+        likeDislikeEntityIds,
+        manager,
+      );
+    }
+
+    return this.blogRepository.deleteById(blogId, manager);
+  }
+}
+
+/*
+export class DeleteBlogWithinTransaction {
   constructor(
     private commentsService: CommentsService,
     private likesDislikesService: LikesCounterBlogsService,
     private blogRepository: BlogRepository,
     @Inject(DataSource) private readonly dataSource: DataSource,
   ) {}
-
   async executeDeleteTransaction(data: IBlogDeleteData): Promise<boolean> {
     const queryRunner = this.dataSource.createQueryRunner();
     await queryRunner.connect();
@@ -55,3 +94,4 @@ export class BlogDeleteTransaction {
     }
   }
 }
+*/

014c047b94a263f2397af935983ace1926ddaf35
Kashyap Solanki <kash.cdac@gmail.com>
2025-05-07 00:17:10 +0530

    add name & username to search dto

--- src/users/dtos/user.search.dto.ts
+++ src/users/dtos/user.search.dto.ts
@@ -1,3 +1,4 @@
+import { IsString } from '@nestjs/class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 import { IUserEntityFilterData } from 'blog-common-1.0';
 import { IsArray, IsOptional, IsPositive } from 'class-validator';
@@ -13,4 +14,26 @@ export class UserSearchDto implements IUserEntityFilterData {
     example: [4],
   })
   id?: number[];
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  @ApiProperty({
+    name: 'name',
+    description: 'Name of the user',
+    type: Array<String>,
+    example: ['Jhon'],
+  })
+  name?: string[];
+
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  @ApiProperty({
+    name: 'username',
+    description: 'username of the user',
+    type: Array<String>,
+    example: ['tony1960s'],
+  })
+  username?: string[];
 }
